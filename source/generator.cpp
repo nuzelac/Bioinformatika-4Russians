56a22d0fae271d8bc300521a2f95863f1e24e4b7
Nino Uzelac <uzelac.nino@gmail.com>
2015-12-15 20:06:38 +0100

    Verzija sa ubrzanim hashiranjem

---
+++ source/generator.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+char alphabet[] = "ACTG";
+int n = 500000;
+
+void printWord() {
+  for(int i = 0; i < n; ++i) {
+    printf("%c", alphabet[rand() % 4]);
+  }
+  printf("\n");
+}
+
+int main() {
+  srand(time(0));
+
+  printWord();
+  printWord();
+}
